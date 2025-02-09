d052fdd6367e5d17f4d9fe1384ce1d8156213d64
ryrl9703 <18762739367@163.com>
2025-02-09 19:36:54 +0800

    CountBadPairs

---
+++ leetcode/2025/Feb/2364.CountNumberofBadPairs/solution.py
@@ -0,0 +1,39 @@
+# -*- encoding: utf-8 -*-
+"""
+@Introduce  : 
+@File       : solution.py
+@Author     : ryrl
+@Email      : ryrl970311@gmail.com
+@Time       : 2025/02/09 18:45
+@Description: 
+"""
+
+from collections import defaultdict
+from typing import List
+
+class Solution:
+    def countBadPairs(self, nums: List[int]) -> int:
+
+        n = len(nums)
+        goodPairs = 0
+        hashMap = defaultdict(int)
+
+        for i in range(n):
+            diff = nums[i] - i
+
+            if diff in hashMap:
+                goodPairs += hashMap[diff]
+            
+            hashMap[diff] += 1
+        
+        totalPairs = n * (n - 1) // 2
+        return totalPairs - goodPairs
+        
+
+
+if __name__ == '__main__':
+
+    nums = [4,1,3,3]
+    s = Solution()
+    s.countBadPairs(nums)
+s
