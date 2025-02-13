95623a7ed1a7229fcc0d5335f188b867b9d31385
ruoyeruolan <ryrl970311@GMAIL.COM>
2025-02-13 21:09:55 +0800

    practicw

---
+++ kaggle/2025/Feb/BackPredictionCahnge.py
@@ -0,0 +1,17 @@
+# -*- encoding: utf-8 -*-
+"""
+@Introduce  : 
+@File       : BackPredictionCahnge.py
+@Author     : ryrl
+@Email      : ryrl970311@gmail.com
+@Time       : 2025/02/11 18:47
+@Description: 
+"""
+
+import pandas as pd
+from category_encoders import TargetEncoder
+from sklearn.model_selection import train_test_split
+
+
+def demo():
+    pass
