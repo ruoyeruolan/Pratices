37afd9a4ca8817af6efdb47cdda21cb0cba3cf83
ruoyeruolan <ryrl970311@GMAIL.COM>
2025-02-13 21:55:29 +0800

    threeSum

--- leetcode/2025/Feb/15.3Sum/solution.py
+++ leetcode/2025/Feb/15.3Sum/solution.py
@@ -12,4 +12,45 @@ from typing import List
 
 class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
-        pass
+        """Two Pointers"""
+        nums.sort()
+        n = len(nums)
+        res = []
+        for i in range(n):
+            if i > 0 and nums[i] == nums[i - 1]:
+                continue
+            
+            target = -nums[i]
+            k = n - 1
+            for j in range(i + 1, n):
+                if j > i + 1 and nums[j] == nums[j - 1]:
+                    continue
+                
+                while j < k and nums[j] + nums[k] > target:
+                    k -= 1
+                if j == k:
+                    break
+                if nums[j] + nums[k] == target:
+                    res.append([nums[i], nums[j], nums[k]])
+        return res
+    
+    def threeSum_(self, nums: List[int]) -> List[List[int]]:
+        """Hash Table"""
+
+        nums.sort()
+        n = len(nums)
+        res = set()
+
+        for i in range(n):
+            if i > 0 and nums[i] == nums[i - 1]:  # skip the same number
+                continue
+
+            target = -nums[i]
+            hashtable = set()
+            for j in range(i + 1, n):
+                complement = target - nums[j]
+
+                if complement in hashtable:
+                    res.add((nums[i], nums[j], complement))
+                hashtable.add(nums[j])
+        return [list(triplet) for triplet in res]
