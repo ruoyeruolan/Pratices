c8d8650fb0ed3de6a3ba36a133fbf14902cc2702
ruoyeruolan <ryrl970311@GMAIL.COM>
2025-03-15 17:54:38 +0800

    sync

--- kaggle/RNA3D/gnntransformer.py
+++ kaggle/RNA3D/gnntransformer.py
@@ -6,16 +6,19 @@
 # @Time       : 2025/03/08 18:24
 # @Description: 
 
-from typing import Tuple
+from typing import Tuple, List
 
 import torch
+from torch import mode
 import torch.nn as nn
 import torch.nn.functional as F
 
 from torch import Tensor, device
+from torch.optim import Adam
 from torch.nn import Linear, ModuleList, TransformerEncoder, TransformerEncoderLayer
 
 from torch_geometric.data import Data
+from torch_geometric.loader import DataLoader
 from torch_geometric.nn import GATConv, global_mean_pool
 
 
@@ -51,7 +54,7 @@ class RNA3DTransformerGNN(nn.Module):
         # Final linear layer for coordinate prediction
         self.linear_pred = Linear(in_features=hidden_channels * nheads, out_features=3)  # out is the 3D coordinates
     
-    def forward(self, x: Tensor, edge_index: Tensor, batch: Tensor) -> Tuple[Tensor, Tensor]:
+    def forward(self, x: Tensor, edge_index: Tensor, batch: Tensor) -> Tensor:
 
         for conv in self.convs:
             x = conv(x, edge_index)
@@ -76,18 +79,63 @@ class RNA3DTransformerGNN(nn.Module):
         x_padding = x_padding.transpose(0, 1)
 
         coords = self.linear_pred(x_padding)
-        coords_flat = torch.cat([coords[i, :seq_lens] for i in range(batch_size)], dim=0)
-        pooled_coords = global_mean_pool(coords_flat, batch)
-        return coords, pooled_coords
+        # coords_flat = torch.cat([coords[i, :seq_lens] for i in range(batch_size)], dim=0)
+        # pooled_coords = global_mean_pool(coords_flat, batch)
+        return coords
+
+def main(dataList: List[Data]):
+
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+    # Check if data is not empty and has valid x attribute
+    if dataList and hasattr(dataList[0], 'x') and dataList[0].x is not None:
+        in_channels = dataList[0].x.size(-1)
+    else:
+        raise ValueError("Input data must contain at least one element with valid features")
+
+    data_loader = DataLoader(dataList, batch_size=8, shuffle=True)
+
+    model = RNA3DTransformerGNN(
+        in_channels=in_channels,
+        hidden_channels=32,
+        device=device,
+    ).to(device=device)
+
+    criterion = nn.MSELoss()
+    optimizer = Adam(params=model.parameters(), lr=0.001)
+
+    epoches = 100
+    for epoch in range(epoches):
+        
+        model.train()
+        loss = 0.0
+
+        for loader in data_loader:
+            data = loader.to(device=device)
+
+            x, edge_indx, batch, y = data.x, data.edge_index, data.batch, data.y
+
+            optimizer.zero_grad()
+
+            coords = model(x, edge_indx, batch)
+            seq_lens = torch.bincount(batch)
+            coords_flat = []
+            # start_idx = 0
+            for i, seq_len in enumerate(seq_lens):
+                coords_flat.append(coords[i, :int(seq_len)])
+                # start_idx += seq_len
+            coords_flat = torch.cat(coords_flat, dim=0)
+
+            loss_ = criterion(coords_flat, y)
+            loss_.backward()
+            optimizer.step()
+
+            loss += loss_.item() * data.num_graphs
+
+        epoch_loss = loss / len(dataList)
+        print(f"Epoch [{epoch}/{epoches}], Loss: {epoch_loss:.4f}")
 
 
-def nucleotide_encoder(nucleotide):
-    mapping = {'A': [1, 0, 0, 0], 'C': [0, 1, 0, 0], 'G': [0, 0, 1, 0], 'U': [0, 0, 0, 1]}
-    return mapping.get(nucleotide.upper(), [0, 0, 0, 0])
+if __name__ == '__main__':
 
-import pandas as pd
-
-pd.Timestamp('2022-05-27')
-pd.to_datetime('2022-05-27')
-
-from scipy.spatial.distance import pdist, squareform
+    pass
--- kaggle/RNA3D/preprocess.py
+++ kaggle/RNA3D/preprocess.py
@@ -4,18 +4,156 @@
 # @Author     : ryrl
 # @Email      : ryrl970311@gmail.com
 # @Time       : 2025/03/10 22:23
-# @Description: 
+# @Description:
 
+
+import bisect
+import numpy as np
 import pandas as pd
+from tqdm import tqdm
+
+from typing import List
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+from collections import defaultdict
+from scipy.spatial.distance import pdist, squareform
+
+from torch_geometric.data import Data
+from torch_geometric.loader import DataLoader
 
 
-def preprocess():
-    train = pd.read_csv('/Users/wakala/IdeaProjects/Practices/kaggle/train_sequences.csv')
+def preprocess(fname: str = 'train'):
+    train = pd.read_csv(f'./data/RNA3D/{fname}_sequences.csv')
     dit = train.set_index('target_id').to_dict()
-    labels = pd.read_csv('/kaggle/input/stanford-rna-3d-folding/train_labels.csv')
-    labels['ID'] = labels['ID'].str.replace('_\d+$', '', regex=True)
-    labels = labels.assign(**{key: labels['ID'].map(val) for key, val in dit.items()})
+    
+    labels = pd.read_csv(f'./data/RNA3D/{fname}_labels.csv')
+    labels['ID'] = labels['ID'].str.replace(r'_\d+$', '', regex=True)
+    labels = labels.assign(**{str(key): labels['ID'].map(val) for key, val in dit.items()})
     labels = pd.concat([labels.iloc[:, 0], labels.iloc[:, 6], labels.iloc[:, 1:6], labels.iloc[:, 7:]], axis=1)
     labels['temporal_cutoff'] = labels['temporal_cutoff'].apply(pd.to_datetime)
-    # labels.head()
     return labels
+
+def pairing_rules(sequence: str, edges: list, nodes_number: list) -> list:
+    pairs = {
+        ('A','U'), ('U','A'),
+        ('G','C'), ('C','G'),
+        ('G','U'), ('U','G')
+        }
+
+    base_positions = defaultdict(list)
+    for idx, base in enumerate(sequence):
+        if idx in nodes_number:
+            base_positions[base].append(idx)
+
+    for (b1, b2) in pairs:
+        list1_sorted = sorted(base_positions[b1])
+        list2_sorted = sorted(base_positions[b2])
+
+        for i in list1_sorted:
+            pos = bisect.bisect_right(list2_sorted, i + 1)
+            for j in list2_sorted[pos:]:
+                edges.append((i, j))
+                edges.append((j, i))
+    # base_positions = defaultdict(list)
+    # for idx, base in enumerate(sequence):
+    #     base_positions[base].append(idx)
+    
+    # # for base in base_positions:
+    # #     base_positions[base] = np.array(base_positions[base], dtype=np.int32)
+
+    # base_arrays = {base: np.array(positions, dtype=np.int32) for base, positions in base_positions.items()}
+
+    # for (b1, b2) in pairs:
+    #     arr1 = base_arrays[b1] # if b1 in base_arrays else np.array([], dtype=np.int32)
+    #     arr2 = base_arrays[b2] # if b2 in base_arrays else np.array([], dtype=np.int32)
+        
+    #     if arr1.size == 0 or arr2.size == 0:
+    #         continue
+        
+    #     i = arr1[:, None]   # shape (len(arr1), 1)
+    #     j = arr2[None, :]   # shape (1, len(arr2))
+        
+    #     mask = (j > i + 1)
+        
+    #     i_idx, j_idx = np.where(mask)
+        
+    #     src = arr1[i_idx]
+    #     dst = arr2[j_idx]
+        
+    #     edges.extend(zip(src, dst))
+    #     edges.extend(zip(dst, src))
+    return edges
+
+def create_rna_graph(
+        data: pd.DataFrame, 
+        # use_spatial_distance: bool = True,
+        use_pairing_rules: bool = True, 
+        # distance_threshold: float = 12.0, 
+        embedding_dim: int = 64
+):
+    base_to_int = {'A': 0, 'C': 1, 'G': 2, 'U': 3, 'X': 4, '-': 4}
+    num_bases = 5
+
+    num_nodes = maxLength = len(data['sequence'].iloc[0])
+    embeddingLayer = nn.Embedding(maxLength + 1, embedding_dim=embedding_dim)
+
+    # Apply vectorized operations
+    bases = torch.tensor(data['resname'].map(base_to_int).values, dtype=torch.long)
+    resids = torch.tensor(data['resid'].values, dtype=torch.long)
+
+    base_one_hot = F.one_hot(bases, num_classes=num_bases).float()
+    resid_embedding = embeddingLayer(resids)
+
+    node_features = torch.cat([base_one_hot, resid_embedding], dim=1)
+    
+    # num_nodes = data.shape[0]
+    edges = []
+    # if use_spatial_distance and all(col in data.columns for col in ['x_1', 'y_1', 'z_1']):
+        
+    #     coords = data[['x_1', 'y_1', 'z_1']].values
+    #     distances = squareform(pdist(coords))
+
+    #     src, dst = np.where((
+    #         distances < distance_threshold) & 
+    #         (np.triu(np.ones_like(distances), k=1) > 0)
+    #     )
+
+    #     edges.extend(zip(src, dst))
+    #     edges.extend(zip(dst, src))
+        
+    nodes_number = data['resid'].to_list()
+    for i in range(num_nodes - 1):
+        if i in nodes_number and i + 1 in nodes_number:
+            edges.append((i, i + 1))
+            edges.append((i + 1, i))
+    
+    if use_pairing_rules:
+        sequence = data['sequence'].iloc[0]
+        edges = pairing_rules(sequence=sequence, edges=edges, nodes_number=nodes_number)
+        
+    edges = list(set(edges))
+    edge_index = torch.tensor(edges, dtype=torch.long).t().contiguous()
+    
+    graph_data = Data(x=node_features, edge_index=edge_index)
+
+    if 'x_1' in data.columns:
+        graph_data.y = torch.tensor(data[['x_1', 'y_1', 'z_1']].values, dtype=torch.float)
+
+    graph_data.sequence = sequence
+    graph_data.ID = data['ID'].iloc[0]
+    return graph_data
+
+
+
+if __name__ == '__main__':
+
+    data = preprocess()
+    df = data.groupby(by='ID')
+    graph_list = [create_rna_graph(data=group) for _, group in tqdm(df, total=len(df))]
+
+    train_loader = DataLoader(graph_list, batch_size=8, shuffle=True)
+    # in_channel = graph_list[0].x.size(-1)
+
